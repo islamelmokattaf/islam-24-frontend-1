912a2e215f15950836ea74f5bda2291498d42bb3
Islam <islam.mapanalyst@gmail.com>
2026-04-04 04:51:06 +0300

    add cloudinary domain to next.js image config

--- next.config.js
+++ next.config.js
@@ -19,6 +19,11 @@ const nextConfig = {
         hostname: process.env.STRAPI_MEDIA_HOST || "localhost",
         pathname: "/uploads/**",
       },
+{
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
     ],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
